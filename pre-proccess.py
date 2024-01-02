21d38cac153ecb599f4542e01bca0c358548c09d
Paul <alphagoo@g-mail.nsysu.edu.tw>
2024-01-03 00:26:06 +0800

    can execute pre-process.py without openpose bin

--- pre-proccess.py
+++ pre-proccess.py
@@ -1,27 +1,26 @@
 #!/usr/bin/env python
-import os
-import numpy as np
-from processor import demo
-
 import glob
+import os
+
+import numpy as np
+
+from processor import demo
 
 
 def get_all_videos(path):
-	
-	files = [f for f in glob.glob(path + "/**/*.avi", recursive=True)]
+    files = [f for f in glob.glob(path + "/**/*.mp4", recursive=True)]
 
-	return files
+    return files
 
 
 db = "UrFall"
+path_db = "Data"
+path_save = os.path.join("Myfeatures", db)
 
-path_db = "./Bases/"+db
-path_save = "./features/"+db+"/"
+ur_adls = os.path.join(path_db, "ADLs")
+print(f"ADLs: {ur_adls}")
 
-
-print(path_db+"/NoFall/")
-
-classes = os.listdir(path_db+"/NoFall/")
+classes = os.listdir(ur_adls)
 classes.sort()
 
 
@@ -34,18 +33,18 @@ print(files)
 d = demo.Demo()
 np_db = []
 n_features = 256
-for i,file in enumerate(files):
-	print(i,"de",len(files))
+for i, file in enumerate(files):
+    print(i, "de", len(files))
 
-	tipo = classe_name = file.split("/")[-3]
-	if tipo=="NoFall":
-		classe_name = file.split("/")[-2]
-		index = np.array(classes.index(classe_name))
-	else:
-		index = np.array(len(classes))
-	f3 = d.start(file,n_features=n_features)
-	f3 = np.hstack((f3,index))
-	np_db.append(f3)
+    tipo = classe_name = file.split("/")[-3]
+    if tipo == "ADLs":
+        classe_name = file.split("/")[-2]
+        index = np.array(classes.index(classe_name))
+    else:
+        index = np.array(len(classes))
+    f3 = d.start(file, n_features=n_features)
+    f3 = np.hstack((f3, index))
+    np_db.append(f3)
 
 np_db = np.array(np_db)
-np.save(path_save+str(n_features)+"full",np_db)
+np.save(path_save + str(n_features) + "full", np_db)
